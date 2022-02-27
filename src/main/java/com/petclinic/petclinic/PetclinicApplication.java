86fb8e72cc283b6a0d5fb3b64294865da1825bf9
h1dd33n <kistjantampere@gmail.com>
2022-02-27 09:49:10 +0200

    closes #2

---
+++ src/main/java/com/petclinic/petclinic/PetclinicApplication.java
@@ -0,0 +1,19 @@
+package com.petclinic.petclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@SpringBootApplication
+public class PetclinicApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(PetclinicApplication.class, args);
+
+    }
+
+}
---
+++ src/main/java/com/petclinic/petclinic/model/Vet.java
@@ -0,0 +1,5 @@
+package com.petclinic.petclinic.model;
+
+public class Vet extends Person {
+
+}
